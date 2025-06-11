f3b1946df0960b7d19dd89eb576bf48fb361a43c
ZhihaoLai-MoClo <nicolas.lai@hotmail.com>
2025-06-12 02:39:46 +1000

    Update ST_moclo.py

--- ST_moclo.py
+++ ST_moclo.py
@@ -6,6 +6,7 @@ from io import StringIO
 import re
 import requests
 import random
+from datetime import datetime
 
 #from xml.etree import ElementTree
 import re
@@ -14,29 +15,31 @@ from functions import restriction_sites, amino_acid_to_codon, codon_to_aa, get_c
 st.set_page_config(page_title="MoClo-YTK Assembly Tool", layout="wide")
 st.title("MoClo Cloning Platform")
 st.markdown("""
-<style>
-.aligned-paragraph {
-    width: 100%;
-    font-family: 'Lucida Handwriting', monospace;
-    font-size: 16px;
-    line-height: 1.6;
-    text-align: justify;
-    text-indent: 0; /* No indent on first line */
-}
-.aligned-paragraph::first-line {
-    text-align: left;
-}
-.aligned-paragraph span {
-    display: block;
-    text-align: right;
-}
-</style>
+        <style>
+        .aligned-paragraph {
+            width: 100%;
+            font-family: 'Lucida Handwriting', monospace;
+            font-size: 16px;
+            line-height: 1.6;
+            text-align: justify;
+            text-indent: 0; /* No indent on first line */
+        }
+        .aligned-paragraph::first-line {
+            text-align: left;
+        }
+        .aligned-paragraph span {
+            display: block;
+            text-align: right;
+        }
+        </style>
 
-<p class="aligned-paragraph">
-    MoClo is hard to learn, but easy to build plasmids at the bench.
-    <span>---- Prof. Chen Davidovich</span>
-</p>
-""", unsafe_allow_html=True)
+        <p class="aligned-paragraph">
+            MoClo is hard to learn, but easy to build plasmids at the bench.
+            <span>---- Prof. Chen Davidovich</span>
+        </p>
+        """, unsafe_allow_html=True)
+
+    
 
 HELPER_RULES = [
     ("pL6", ["Pos1", "Pos2", "Pos3", "Pos4", "Pos5"]),
@@ -68,10 +71,10 @@ page = st.sidebar.selectbox("Select function", [
     "Level 2 Assembly", 
     "Generate level 0 parts",
     "Tables editor",
+    
     ])
 
 uploaded_file = st.file_uploader("Upload CSV (name, type, sequence, no_tag_sequence, parts_used)", type="csv")
-
 try:
     if uploaded_file:
         df = pd.read_csv(uploaded_file)
@@ -86,80 +89,85 @@ except Exception as e:
 
 
 if page == "Generate level 0 parts":
+    
         
-        Subpage = st.sidebar.selectbox("Select sequence type", [
+    Subpage = st.sidebar.selectbox("Select sequence type", [
         "Coding (ORFs, Tags)",  "Non-coding (Promoters, Terminators)",
     ])
-        if Subpage == "Coding (ORFs, Tags)":
-            st.header("UniProt isoform to CCDS link")
-            uniprot_id = st.text_input("Enter UniProt ISOFORM ID (e.g., Q92833-1 not Q92833)")
+    if Subpage == "Coding (ORFs, Tags)":
+        st.sidebar.title (":bulb: Tips! :bulb:")
+        st.sidebar.subheader ("UniProt isoform DNA squence finder")
+        st.sidebar.markdown("UniProt isoform DNA squence finder will help you find the DNA sequence of a protein using its uniprot ID. Make sure you enter the ID including the isoform number.")
+        st.header("UniProt isoform DNA squence finder")
+        uniprot_id = st.text_input("Enter UniProt ISOFORM ID (e.g., Q92833-1 not Q92833)")
 
-            if st.button("Find DNA and protein sequence"):
-                with st.spinner("Fetching CCDS link..."):
-                    ccds_link, error = get_ccds_link(uniprot_id)
-                    if error:
-                        st.error(error)
-                    else:
-                        st.success("CCDS link found! Please double check!")
-                        st.markdown(f"[Open CCDS entry for {uniprot_id}]({ccds_link})")
+        if st.button("Find DNA and protein sequence"):
+            with st.spinner("Fetching CCDS link..."):
+                ccds_link, error = get_ccds_link(uniprot_id)
+                if error:
+                    st.error(error)
+                else:
+                    st.success("CCDS link found! Please double check!")
+                    st.markdown(f"[Open CCDS entry for {uniprot_id}]({ccds_link})")
             
-            st.header("Restriction site remover")
-            st.markdown("Enter a coding sequence below. The tool will remove stop codon at the end, BsaI, BsmBI, NotI and PmeI sites.")
-            dna_input = st.text_area("Input DNA Sequence (ATGC only)", height=200).strip().upper()
-            dna_input = re.sub(r'\s+', '', dna_input.upper())
-            if dna_input.endswith("TAA") or dna_input.endswith("TAG") or dna_input.endswith("TGA"):
-                    dna_input = dna_input[:-3]  # remove stop codon
+        st.header("Restriction sites remover")
+        st.sidebar.subheader ("Restriction sites remover")
+        st.sidebar.markdown("Restriction sites remover tool will remove stop codon at the end, BsaI, BsmBI, NotI and PmeI retriction sites by silent mutations." \
+        "At the end of this process, you have an option to add the sequence directly to the CSV file, but only the BsaI sites will be added. Backbone sequences are not added to reduce file size."
+        )
+        
+        dna_input = st.text_area("Input DNA Sequence (ATGC only)", height=200).strip().upper()
+        dna_input = re.sub(r'\s+', '', dna_input.upper())
+        dna_input = rm_stop_codon(dna_input)
 
-            if dna_input:
-                site_counts = count_re_sites(dna_input)
-                st.subheader("Restriction Site Count in Original Sequence")
-                for enzyme, count in site_counts.items():
-                    st.write(f"🔹 {enzyme}: {count} site(s)")
-                rm_stop_codon(dna_input)
-                protein, stops = translate_and_find_stops(dna_input)
-                if len(stops) > 1:
-                    st.error(f"Internal stop codon found at position {stops}")
-                    st.text(protein)
+        if dna_input[:3] not in ['ATG']:
+            st.text(f"No ATG found!")
+
+        if dna_input:
+            site_counts = count_re_sites(dna_input)
+            st.subheader("Restriction Site Count in Original Sequence")
+            for enzyme, count in site_counts.items():
+                st.write(f"🔹 {enzyme}: {count} site(s)")
             
-            if dna_input[:3] not in ['ATG']:
-                st.text(f"No ATG found!")
-            
-            
-            if st.button("Show restriction sites"):
+            protein, stops = translate_and_find_stops(dna_input)
+            if len(stops) > 1:
+                st.error(f"Internal stop codon found at position {stops}")
+                st.text(protein)
+
+        if st.button("Show restriction sites"):
                 result = highlight_dna_and_protein(dna_input)
                 st.markdown(result, unsafe_allow_html=True)
-            
-            if st.button("Visualise optimised DNA"):
-                if not dna_input or not re.fullmatch(r'[ATGC]+', dna_input):
-                    st.error("Please enter a valid DNA sequence containing only A, T, G, C.")
-                else:
-                    st.text(f"Original sequence length: {len(dna_input)} bp")
-                    dna_cleaned = rm_stop_codon(dna_input)
-                    dna_optimized = optimize_dna_sequence(dna_cleaned)
-                    st.session_state.dna_optimized = dna_optimized
-                    st.success("Optimized DNA Sequence (Restriction sites removed)")
-                    st.code(dna_optimized, language="text")
-                    st.text(f"Optimized sequence length: {len(dna_optimized)} bp")
-                    site_counts = count_re_sites(dna_optimized)
-                    st.subheader("Restriction Site Count again")
-                    for enzyme, count in site_counts.items():
-                        st.write(f"🔹 {enzyme}: {count} site(s)")
-                    optimised_result = highlight_dna_and_protein(dna_optimized)
-                    st.markdown(optimised_result, unsafe_allow_html=True)
-            new_name = st.text_input("Part Name")
-            selected_type = st.selectbox("Part Type", ["N-terminal tag", "CDS", "C-terminal tag"])
-            if st.button("Add optimised sequence to CSV file"):
+        if st.button("Visualise optimised DNA"):
+            if not dna_input or not re.fullmatch(r'[ATGC]+', dna_input):
+                st.error("Please enter a valid DNA sequence containing only A, T, G, C.")
+            else:
+                st.text(f"Original sequence length: {len(dna_input)} bp")
                 dna_cleaned = rm_stop_codon(dna_input)
                 dna_optimized = optimize_dna_sequence(dna_cleaned)
-                dna_optimized = rm_start_codon(dna_optimized)
-                overhangs = {
+                st.session_state.dna_optimized = dna_optimized
+                st.success("Optimized DNA Sequence (Restriction sites removed)")
+                st.code(dna_optimized, language="text")
+                st.text(f"Optimized sequence length: {len(dna_optimized)} bp")
+                site_counts = count_re_sites(dna_optimized)
+                st.subheader("Restriction Site Count again")
+                for enzyme, count in site_counts.items():
+                    st.write(f"🔹 {enzyme}: {count} site(s)")
+                optimised_result = highlight_dna_and_protein(dna_optimized)
+                st.markdown(optimised_result, unsafe_allow_html=True)                       
+        new_name = st.text_input("Part Name")
+        selected_type = st.selectbox("Part Type", ["N-terminal tag", "CDS", "C-terminal tag"])
+        if st.button("Add optimised sequence to CSV file"):
+            dna_cleaned = rm_stop_codon(dna_input)
+            dna_optimized = optimize_dna_sequence(dna_cleaned)
+            dna_optimized = rm_start_codon(dna_optimized)
+            overhangs = {
                         "N-terminal tag": ("TCGGTCTCAAAGATG", "TCCGGTATGTGAGACC"),
                         "CDS": ("TCGGTCTCATATG", "GGATCCTGAGACC"),
                         "C-terminal tag": ("TCGGTCTCAATCCTCAGGT", "TCCGGTGGCTGAGACC"),
                     }
-                left_oh, right_oh = overhangs.get(selected_type, ("", ""))
-                final_seq = left_oh + dna_optimized + right_oh
-                new_row = pd.DataFrame([{
+            left_oh, right_oh = overhangs.get(selected_type, ("", ""))
+            final_seq = left_oh + dna_optimized + right_oh
+            new_row = pd.DataFrame([{
                         "name": new_name,
                         "type": selected_type,
                         "sequence": final_seq,
@@ -167,62 +175,72 @@ if page == "Generate level 0 parts":
                         "parts_used": ""
                     }])
                     
-                df = pd.concat([df, new_row], ignore_index=True)
-                csv = df.to_csv(index=False)
-                st.success("New part added with overhangs")
-                st.download_button("Download updated CSV of Level 0 parts", csv, "updated_parts.csv", "text/csv")
+            df = pd.concat([df, new_row], ignore_index=True)
+            csv = df.to_csv(index=False)
+            st.success("New part added with overhangs")
+            st.download_button("Download updated CSV of Level 0 parts", csv, "updated_parts.csv", "text/csv")
         
-        elif Subpage == "Non-coding (Promoters, Terminators)":
-            st.header("Add new sequence")
-            new_name = st.text_input("Part Name")
-            new_seq = st.text_area("DNA sequence")
-            if new_seq:
-                site_counts = count_re_sites(new_seq)
-                st.subheader("Restriction sites count")
-                for enzyme, count in site_counts.items():
-                    st.write(f"🔹 {enzyme}: {count} site(s)")
+    elif Subpage == "Non-coding (Promoters, Terminators)":
+        st.header("Add new sequence")
+        st.sidebar.title (":bulb: Tips! :bulb:")
+        st.sidebar.markdown("This tool can be used to add parts directly. It will check the restriction site but wont try to remove it. " \
+        "For promoter and terminator, it will also add no tag sequence. " \
+        "At the end of this process, you have an option to add the sequence directly to the CSV file, but only the BsaI sites will be added. " \
+        "Backbone sequences are not added to reduce file size.")
+        new_name = st.text_input("Part Name")
+        new_seq = st.text_area("DNA sequence")
+        if new_seq:
+            site_counts = count_re_sites(new_seq)
+            st.subheader("Restriction sites count")
+            for enzyme, count in site_counts.items():
+                st.write(f"🔹 {enzyme}: {count} site(s)")
                 
-            selected_type = st.selectbox("Part Type", ["Promoter", "N-terminal tag", "CDS", "C-terminal tag", "Terminator"])
+        selected_type = st.selectbox("Part Type", ["Promoter", "N-terminal tag", "CDS", "C-terminal tag", "Terminator"])
 
-            if st.button("Add Overhangs"):
-                overhangs = {
+        if st.button("Add Overhangs"):
+            overhangs = {
                     "Promoter": ("TCGGTCTCAAACG", "GGAAGATGAGACC"),
                     "N-terminal tag": ("TCGGTCTCAAAGATG", "TCCGGTATGTGAGACC"),
                     "CDS": ("TCGGTCTCATATG", "GGATCCTGAGACC"),
                     "C-terminal tag": ("TCGGTCTCAATCCTCAGGT", "TCCGGTGGCTGAGACC"),
                     "Terminator": ("TCGGTCTCATGGCTAATGA", "GGGCTGTGAGACC")
                 }
-                no_tag_overhangs = {
+            no_tag_overhangs = {
                     "Promoter": ("TCGGTCTCAAACG", "TCCGGTATGTGAGACC"),
                     "Terminator": ("TCGGTCTCAATCCTAATGA", "GGGCTGTGAGACC")
                 }
 
-                left_oh, right_oh = overhangs.get(selected_type, ("", ""))
-                if selected_type in ["N-terminal tag", "CDS", "C-terminal tag"]:
+            left_oh, right_oh = overhangs.get(selected_type, ("", ""))
+            if selected_type in ["N-terminal tag", "CDS", "C-terminal tag"]:
                     st.text(f"Only stop codons will be removed")
                     new_seq = rm_start_codon(new_seq) #added back in OH
                     new_seq = rm_stop_codon(new_seq)
-                final_seq = left_oh + new_seq + right_oh
+            final_seq = left_oh + new_seq + right_oh
 
-                if selected_type in ["Promoter", "Terminator"]:
-                    final_notag_seq = no_tag_overhangs[selected_type][0] + new_seq + no_tag_overhangs[selected_type][1]
-                else:
-                    final_notag_seq = ""
+            if selected_type in ["Promoter", "Terminator"]:
+                final_notag_seq = no_tag_overhangs[selected_type][0] + new_seq + no_tag_overhangs[selected_type][1]
+            else:
+                final_notag_seq = ""
                 
-                new_row = pd.DataFrame([{
+            new_row = pd.DataFrame([{
                         "name": new_name,
                         "type": selected_type,
                         "sequence": final_seq,
                         "no_tag_sequence": final_notag_seq,
                         "parts_used": ""
                     }])
-                df = pd.concat([df, new_row], ignore_index=True)
-                csv = df.to_csv(index=False)
-                st.success("New part added with overhangs")
-                st.download_button("Download updated CSV of level 0 parts", csv, "updated_parts.csv", "text/csv")
+            df = pd.concat([df, new_row], ignore_index=True)
+            csv = df.to_csv(index=False)
+            st.success("New part added with overhangs")
+            st.download_button("Download updated CSV of level 0 parts", csv, "updated_parts.csv", "text/csv")
    
 if page == "Level 1 Assembly": 
         st.header("Level 1 Assembly")
+        st.sidebar.title (":bulb: Tips! :bulb:")
+        st.sidebar.markdown ("This tool will help you contruct many level 1 plasmids all at once. " \
+        "The option to change promoters or terminators has been removed, since there is only one option for each. " \
+        "It will atomatically select the no_tag sequence when no N- or C-terminal tags are added."
+        "At the end of this process the level 1 plasmid will be digested by BsmBI and stored in the CSV file to reduce file size.")
         num_constructs = st.number_input("Number of constructs", min_value=1, max_value=10)
         types = ["Promoter", "N-terminal tag", "CDS", "C-terminal tag", "Terminator"]
         constructs = []
@@ -312,6 +330,8 @@ if page == "Level 1 Assembly":
 
 elif page == "Level 2 Assembly":
         st.header("Level 2 Assembly")
+        st.sidebar.title (":bulb: Tips! :bulb:")
+        st.sidebar.markdown ("This tool will help you contruct level 2 plasmids and show linear maps of them. When finding level 1 plasmids you can use the simpified map to check if they are the one you want.")
         pos_cols = [f"Pos{i}" for i in range(1, 7)]
         lv2bb_options = df[df["type"].str.contains("Lv2BB")]["name"].tolist()
         filtered_df = df[df["type"].str.contains("Pos|Lv2BB") | df["name"].isin([h for h, _ in HELPER_RULES])]
@@ -381,7 +401,7 @@ elif page == "Level 2 Assembly":
                 export_txt.write(f">{row['name']}\n{row['sequence']}\n")
             st.download_button("Download FASTA file of Level 2 plasmids", export_txt.getvalue(), "assembled_level2_sequences.fasta", "text/plain")
 
-            # Define a color map for consistent part coloring
+            
             color_map = {}
 
             def get_random_color():
@@ -411,10 +431,13 @@ elif page == "Level 2 Assembly":
                 ax.axis('off')
                 st.pyplot(fig)
 
+
 elif page == "Tables editor":
         st.title("Edit Sequences & Batch Download FASTA")
-        
-        
+        st.sidebar.title (":bulb: Tips! :bulb:")
+        st.sidebar.markdown ("This tool allow you to edit the CSV file directly and export selcted parts as FSTA files.")
+        st.sidebar.title (":warning: Warning! :warning:")
+        st.sidebar.markdown ("There is no fail safe mechanism build in for this tool. Please be caucious when you edit sequence. Level 0 and level 1 plasmids are stored as digested form to reduce file size. The sequences are incomplete.")
         st.subheader("Edit Sequences")
         edited_df = st.data_editor(df, num_rows="dynamic", use_container_width=True)
 
@@ -430,4 +453,28 @@ elif page == "Tables editor":
             st.download_button("Download selected sequences as FASTA", fasta_output.getvalue(), "selected_sequences.fasta", "text/plain")
         csv_output = edited_df.to_csv(index=False)
         st.download_button("Download Edited CSV", csv_output, "edited_sequences.csv", "text/csv")
-    
+
+
+bottom_sidebar = st.sidebar.empty()
+bottom_sidebar.markdown("""---  
+**Useful Links**  
+- [Check the code on Github](https://github.com/ZhihaoLai-MoClo/MoClo_Baculo_toolkit/)  
+- [Cite our MoClo Baculo paper](https://www.sciencedirect.com/science/article/pii/S0022283625000099)  
+- [Get MoClo Baculo toolkit from Addgene](https://www.addgene.org/kits/davidovich-moclo-baculo/)  
+- [Get protocol](https://www.davidovich-lab.com/moclobaculo)  
+- [Davidovich Lab](https://www.davidovich-lab.com/)  
+""")
+
+api_url = f"https://api.github.com/repos/ZhihaoLai-MoClo/MoClo_Baculo_toolkit/commits"
+try:
+    response = requests.get(api_url)
+    response.raise_for_status()
+    commits = response.json()
+    latest_commit_date = commits[0]["commit"]["committer"]["date"]
+    # Format nicely
+    date_obj = datetime.strptime(latest_commit_date, "%Y-%m-%dT%H:%M:%SZ")
+    st.sidebar.markdown("Made by Zhihao Lai")
+    st.sidebar.markdown(f"**Last updated on GitHub:** {date_obj.strftime('%Y-%m-%d %H:%M')} UTC")
+except Exception as e:
+    st.sidebar.warning("Could not fetch update date on GitHub.")
+    st.sidebar.text(f"Error: {e}")
