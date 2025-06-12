0afa4d2b5ab9d9c6d7c7ce565fa0b5d050833a1c
ZhihaoLai-MoClo <nicolas.lai@hotmail.com>
2025-06-12 13:31:08 +1000

    Update ST_moclo.py

--- ST_moclo.py
+++ ST_moclo.py
@@ -442,14 +442,31 @@ elif page == "Tables editor":
         st.subheader("Edit Sequences")
         edited_df = st.data_editor(df, num_rows="dynamic", use_container_width=True)
 
-        # Multi-select for download
+        
         st.subheader("Select Sequences to Download as FASTA")
         selected_names = st.multiselect("Choose entries to export", edited_df["name"].tolist())
 
         if selected_names:
+            row_000 = df[df["name"] == ["pM.000"]].iloc[0]
+            seq_000 = BsmBI_dg(row_000['sequence'])
             fasta_output = StringIO()
             for _, row in edited_df[edited_df["name"].isin(selected_names)].iterrows():
-                fasta_output.write(f">{row['name']}\n{row['sequence']}\n")
+                if any(x in row["type"] for x in ["Promoter", "N-terminal tag", "CDS", "C-terminal tag", "Terminator"]):
+                    out_part = []
+                    out_part.append(BsmBI_dg(row['sequence']))
+                    out_part.append(seq_000)
+                    full_seq = ''.join(out_part)
+                    fasta_output.write(f">{row['name']}\n{full_seq}\n")
+                elif any(x in row["type"] for x in ["pMB.pos1","pMB.pos2","pMB.pos3","pMB.pos4","pMB.pos5","pMB.pos6"]):
+                    out_part = []
+                    out_part.append(row['sequence'])
+                    bb_used = row["type"]
+                    bb_row = df[df["name"] == bb_used].iloc[0]
+                    out_part.append(BsmBI_dg_rev(bb_row["sequence"]))
+                    full_seq = ''.join(out_part)
+                    fasta_output.write(f">{row['name']}\n{out_part}\n")
+                else:
+                    fasta_output.write(f">{row['name']}\n{row['sequence']}\n")
 
             st.download_button("Download selected sequences as FASTA", fasta_output.getvalue(), "selected_sequences.fasta", "text/plain")
         csv_output = edited_df.to_csv(index=False)
